3b49ede377cc33c792dba757f9375309064110a5
guptasujal07 <guptasujal3020@gmail.com>
2025-07-11 22:47:56 +0530

    commit

--- DPappraoch/CoinChange.java
+++ DPappraoch/CoinChange.java
@@ -1,23 +1,32 @@
-public class Solution {
-    public int count(int coins[], int sum) {
-        int[] dp = new int[sum + 1];
-        dp[0] = 1;
-        
-        for (int coin : coins) {
-            for (int i = coin; i <= sum; i++) {
-                dp[i] += dp[i - coin];
+class Coinchange {
+
+    public int coinChange(int[] coins, int amt) {
+        int n = coins.length;
+        int[][] dp = new int[n][amt + 1];
+
+        // Base case: using only the first coin
+        for (int a = 0; a <= amt; a++) {
+            if (a % coins[0] == 0) {
+                dp[0][a] = a / coins[0];
+            } else {
+                dp[0][a] = (int) 1e9; // Infinity
             }
         }
-        return dp[sum];
+
+        // Fill DP table
+        for (int i = 1; i < n; i++) {
+            for (int a = 0; a <= amt; a++) {
+                int pick = (int) 1e9;
+                if (a >= coins[i]) {
+                    pick = 1 + dp[i][a - coins[i]];
+                }
+                int noPick = dp[i - 1][a];
+                dp[i][a] = Math.min(pick, noPick);
+            }
+        }
+
+        int ans = dp[n - 1][amt];
+        return ans >= (int) 1e9 ? -1 : ans;
     }
 
-    public static void main(String[] args) {
-        Solution sol = new Solution();
-        
-        int[] coins = {1, 2, 3};
-        int sum = 4;
-        
-        int ways = sol.count(coins, sum);
-        System.out.println("Number of ways to make sum " + sum + " is: " + ways);
-    }
 }
